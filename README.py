ccbf62f09ffa36c40add0f188e133a59c05d3374
tiago-johnson <santiago_johnson@brown.edu>
2026-05-16 09:58:57 -0400

    Update README.py

--- README.py
+++ README.py
@@ -3,7 +3,7 @@
 '''
 For this project, I coded my version of the popular card game known as spades.  As an added twist to the instructions in file 'spades_text.py', I decided to give each spades card an ability.  This was my unique spin on this game.
 
-In order to run this code, you will need to copy the repository and plug it into your desired Python platform such as Google CoLab or VSCode.
+In order to run this code, you will need to download the repository and plug it into your desired Python platform such as Google CoLab or VSCode and then run the main game file known as spades_file.py'
 
 
 '''
