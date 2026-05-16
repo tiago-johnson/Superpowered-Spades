621f2312f13635678b34c99edf96a20233c10420
tiago-johnson <santiago_johnson@brown.edu>
2026-05-16 09:31:57 -0400

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,9 @@
+# Superpowered-Spades
+
+'''
+For this project, I coded my version of the popular card game known as spades.  As an added twist to the instructions in file 'spades_text.py', I decided to give each spades card an ability.  This was my unique spin on this game.
+
+In order to run this code, you will need to copy the repository and plug it into your desired Python platform such as Google CoLab or VSCode.
+
+
+'''
